cfe70b2593243240a18e7aacaece65da366a7608
Tiago Ferreira <tiagoferreira@tuta.io>
2020-08-03 21:01:54 +0100

    Adding log

--- app.js
+++ app.js
@@ -4,28 +4,37 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
-
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3,10);
+            let damage = this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
             if (this.checkWinner()) {
                 return;
             }
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             this.monsterAttack();
         },
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10,20);
+            let damage = this.calculateDamage(10,20);
+            this.monsterHealth -= damage;
             if (this.checkWinner()) {
                 return;
             }
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster with special attack for ' + damage
+            });
             this.monsterAttack();
         },
         heal: function() {
@@ -34,14 +43,23 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttack();
         },
         giveUp: function() {
             this.gameIsRunning = false;
         },
         monsterAttack: function() {
-            this.playerHealth -= this.calculateDamage(5,12);
+            let damage = this.calculateDamage(5,12)
+            this.playerHealth -= damage;
             this.checkWinner();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) +1 ,min);
