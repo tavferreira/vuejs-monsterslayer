2cc1f0114ecc965f4e1a7088a2beaf227d465b3c
Tiago Ferreira <tiagoferreira@tuta.io>
2020-08-03 15:07:46 +0100

    Implementing attack function

--- app.js
+++ app.js
@@ -7,10 +7,41 @@ new Vue({
 
     },
     methods: {
-        startGame : function () {
+        startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            let max = 10;
+            let min = 3;
+            let damage = Math.max(Math.floor(Math.random() * max) +1 ,min);
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert("You won!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) +1 ,min);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("You lost!");
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
         }
     }
 });
